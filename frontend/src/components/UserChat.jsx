7dfd49a55eb75cf9599819fff03edb2eee5f3cf6
srigouthamkrishna1 <srigouthamkrishna1@gmail.com>
2024-03-14 00:33:50 +0530

    added styling for message box

--- frontend/src/components/UserChat.jsx
+++ frontend/src/components/UserChat.jsx
@@ -1,9 +1,21 @@
 import { useFetchRecipienUser } from "../hooks/useFetchRecipient";
-
+import userIcon from "../assets/user-icon-svgrepo-com.svg"
 const UserChat = ({ chat, user }) => {
     const { recipientUser } = useFetchRecipienUser(chat, user)
-    console.log("recipientUser", recipientUser);
-    return (<div>{recipientUser?.name}</div>);
+
+    return (<div className="flex h-[10%] m-2 hover:bg-gray-200">
+        <div className="w-[12%] m-1  h-[90%] bg-blue-200 rounded-full flex justify-center align-items-center">
+            <img className="h-full w-[80%]" src={userIcon} />
+        </div>
+        <div className="flex-col ml-5">
+            <div className=" text-l font-bold flex-col">
+                {recipientUser?.name}
+            </div>
+            <div className="mt-2">Text-Message</div>
+
+        </div>
+        <div className="flex-grow text-right ">Time</div>
+    </div>);
 }
 
 export default UserChat;
