4210dde7321a76bcb81bbb997884be2d5a16079c
srigouthamkrishna1 <srigouthamkrishna1@gmail.com>
2024-07-14 20:21:55 +0530

    modified some files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,6 @@ import {
 import { Signup } from "./pages/Signup";
 import { Signin } from "./pages/Signin";
 import { Dashboard } from "./pages/Dashboard";
-import { SendMoney } from "./pages/SendMoney";
 import { AuthContext } from "./context/AuthContext";
 import { ChatContextProvider } from "./context/ChatContext";
 import { useContext } from "react";
@@ -22,8 +21,9 @@ function App() {
           <Route path="/" element={<Signup />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/send" element={<SendMoney />} />
+          {user && <Route path="/dashboard" element={<Dashboard />} />}
+
+
         </Routes>
       </ChatContextProvider>
 
